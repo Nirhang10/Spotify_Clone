10adc0b7ba7be4af6cd296f9b6b4625b4ae7c39a
Nirhang10 <156817911+Nirhang10@users.noreply.github.com>
2025-08-03 15:29:16 +0545

    Add files via upload

---
+++ Spotify/forms.py
@@ -0,0 +1,33 @@
+from django import forms
+from .models import Playlist
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class RegisterForm(forms.Form):
+    username = forms.CharField(max_length=100)
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
+    password2 = forms.CharField(widget=forms.PasswordInput)
+
+    lables = {
+            'username':'Username',
+            'email':'Email',
+            'password1':'Password',
+            'password2':'Repeat Password'
+    }
+
+    
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+    
+    
+    
+class SearchForm(forms.Form):
+    search_query = forms
+
+class PlaylistForm(forms.ModelForm):
+    class Meta:
+        model = Playlist
+        fields = ('name', 'song')
---
+++ Spotify/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.defult, name='index'),
+    path('home/', views.index, name='index'),
+    path('login/', views.login_view, name='login'),
+    path('signup/', views.signup_view, name='signup'),
+    path('search/', views.search_view, name='search'),
+    path('player/<int:track_id>/', views.musicPlayer_view, name='player'),
+    path('playlist/<int:artist_id>/', views.playlist_view, name='playlist'),
+    path('createplaylist/', views.createPlaylist_view, name='create_playlist'),
+]
